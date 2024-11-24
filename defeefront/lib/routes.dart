69fe7d3cf9456942715f76c9c07c5fc5b23df172
Indu <wjsdlsdn1158@naver.com>
2024-11-24 15:07:47 +0900

    [Feat] make search & search_result

--- defeefront/lib/routes.dart
+++ defeefront/lib/routes.dart
@@ -1,10 +1,13 @@
 import 'package:defeefront/screens/headline/headline.dart';
+import 'package:defeefront/screens/search/search.dart';
+import 'package:defeefront/screens/search_result/search_result.dart';
 import 'package:defeefront/widgets/basescreen.dart';
 import 'package:flutter/material.dart';
 
 final routes = {
   '/': (BuildContext context) => Headline(),
-  '/search': (BuildContext context) => Headline(),
+  '/search': (BuildContext context) => Search(),
+  '/searchresult': (BuildContext context) => SearchResult(),
   '/recommend': (BuildContext context) => Headline(),
   // '/my': (BuildContext context) => Headline(),
   '/my': (context) => const BaseScreen(child: Headline()),
