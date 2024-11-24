fa5d08bcca4330448ecd59ad908e6dcf90bb005d
Indu <wjsdlsdn1158@naver.com>
2024-11-24 15:07:47 +0900

    [Feat] make search & search_result

--- defeefront/lib/routes.dart
+++ defeefront/lib/routes.dart
@@ -1,4 +1,6 @@
 import 'package:defeefront/screens/headline/headline.dart';
+import 'package:defeefront/screens/search/search.dart';
+import 'package:defeefront/screens/search_result/search_result.dart';
 import 'package:defeefront/widgets/basescreen.dart';
 import 'package:defeefront/screens/post/post.dart';
 
@@ -6,7 +8,8 @@ import 'package:flutter/material.dart';
 
 final routes = {
   '/': (BuildContext context) => Headline(),
-  '/search': (BuildContext context) => Headline(),
+  '/search': (BuildContext context) => Search(),
+  '/searchresult': (BuildContext context) => SearchResult(),
   '/recommend': (BuildContext context) => Headline(),
   '/my': (BuildContext context) =>
       PostPage(url: "https://pongpongi.tistory.com/47"),
