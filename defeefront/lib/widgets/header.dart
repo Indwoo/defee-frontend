e7721e2aa2a95674b10b21697063a58c88442e8e
CamelIsTheBestConvention <ansdj3523@gmail.com>
2024-12-04 16:42:05 +0900

    [Fix] change header text by page route && add alarm icon and mypage icon

--- defeefront/lib/widgets/header.dart
+++ defeefront/lib/widgets/header.dart
@@ -5,25 +5,56 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+    final currentRoute = ModalRoute.of(context)?.settings.name;
+
+    String headerText;
+    switch (currentRoute) {
+      case '/search':
+        headerText = "검색";
+        break;
+      case '/recommend':
+        headerText = "추천";
+        break;
+      case '/my':
+        headerText = "MY";
+        break;
+      default:
+        headerText = "헤드라인";
+    }
+
     return AppBar(
       centerTitle: true,
-      leading: IconButton(
-        icon: Icon(
-          Icons.arrow_back_ios_new,
-          color: Colors.indigo[900],
-          size: 24,
-        ),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      ),
+      leading: null,
       title: Text(
-        "헤드라인",
+        headerText,
         style: TextStyle(
-            color: Colors.indigo[900],
-            fontWeight: FontWeight.bold,
-            fontSize: 24),
+          color: Colors.indigo[900],
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+        ),
       ),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.notifications,
+            color: Colors.indigo[900],
+          ),
+          onPressed: () {
+            print("알림 아이콘 클릭됨");
+          },
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.indigo[900],
+          ),
+          onPressed: () {
+            print("목록 아이콘 클릭됨");
+          },
+        ),
+      ],
     );
   }
 
